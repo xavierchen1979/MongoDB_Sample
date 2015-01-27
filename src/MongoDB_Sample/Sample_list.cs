1183eec3c5498e58d9cfc99453c03f219f0f87ad
Xavier_Chen <Xavier_Chen@migocorp.com>
2015-01-27 16:20:07 +0800

    Add function find list by name ,  use query.eq

--- src/MongoDB_Sample/Sample_list.cs
+++ src/MongoDB_Sample/Sample_list.cs
@@ -7,7 +7,11 @@
     using Nancy.Responses.Negotiation;
     using Nancy.Validation;
     using MongoDB.Driver;
+    using MongoDB.Driver.Builders; 
     using MongoDB_Sample.Model;
+    using MongoDB.Bson;
+    using System.Web;
+    using Newtonsoft.Json;
 
     public class Sample_list : NancyModule
     {
@@ -15,18 +19,77 @@
 
         public Sample_list()
         {
-            this.Get["index","/index"] = this.InsertUserData;
+            this.Get["adddata", "/adddata"] = this.InsertUserData;
 
-            this.Get["list", "/list"] = parameters =>
+            this.Get["/"] = parameters =>
             { 
                 return Negotiate
                     .WithModel((object)GetUserList(Request.Query))
+                    .WithAllowedMediaRange(mediaRange: "application/json");
+            };
+
+            this.Get["/getListbyname"] = parameters =>
+            {
+                return Negotiate
+                    .WithModel((object)GetUserListByName(Request.Query))
                     .WithAllowedMediaRange(mediaRange: "application/json");
             };
              
             this.connectStr = "mongodb://tester:1234@ds053438.mongolab.com:53438/xsampledb";
         }
  
+        private RspUser GetUserListByName(dynamic p)
+        {
+            RspUser rspUser;
+
+            if (p.ContainsKey("req"))
+            {
+                //req={"name": "9"} 
+
+                var queryStr = JsonConvert.DeserializeObject(p["req"]);
+
+                string qName = queryStr.name;
+
+                var server = MongoServer.Create(this.connectStr);
+
+                var db = server.GetDatabase("xsampledb");
+
+                MongoCollection<UserModel> coll = db.GetCollection<UserModel>("Users");
+
+                List<RspUserResult> newResultList = new List<RspUserResult>();
+
+                var query = Query.EQ("Name", new BsonRegularExpression(qName));
+
+                foreach (var i in coll.Find(query))
+                {
+                    RspUserResult newResult = new RspUserResult();
+                    newResult.Age = i.Age;
+                    newResult.Name = i.Name;
+                    newResult.Id = i.Id.ToString();
+                    newResultList.Add(newResult);
+                }
+
+                server.Disconnect();
+
+                rspUser = new RspUser
+                {
+                    code = 1,
+                    msg = "Success",
+                    result = newResultList
+                };
+            }
+            else
+            {
+                rspUser = new RspUser
+                {
+                    code = 0,
+                    msg = "Failed",
+                    result = null
+                };
+            }
+            
+            return rspUser;
+        }
 
         private RspUser GetUserList(dynamic p)
         {
